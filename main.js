7d5b1e84e48396cb56a66cf68454fd6a8e6703b3
gOK-m <71651713+gOK-m@users.noreply.github.com>
2021-04-21 17:26:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-function preload(){
 
-}
-function setup(){
+function setup() {
     canvas = createCanvas(640, 480);
-    canvas.position(150, 290);
+    canvas.position(110, 250);
     video = createCapture(VIDEO);
     video.hide();
-    tint_color = "";
 }
-function draw(){
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
+
+function draw() {
+    image(video, 0, 0, 220, 200);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
 }
-function filter_tint(){
-    tint_color = document.getElementById("color_name").value;
-}
-function take_snapshot(){
-    save("gokul.png");
+function takeSnapshot(){
+    save("pic1.png");
 }
